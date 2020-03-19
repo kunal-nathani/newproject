b9be0a7a741aeced247e75c47ddab4682c5c75e4
kunal-nathani <48233889+kunal-nathani@users.noreply.github.com>
2020-03-19 16:29:03 +0530

    Add files via upload

---
+++ _header.php
@@ -0,0 +1,10 @@
+        <div class="header">
+            <h1>Efficient Doctor Patient Portal</h1>
+              <div class="img1" align="left"><image src="logo.jpg"></div>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- The form -->
+<form class="example" action="action_page.php">
+  <input type="text" placeholder="Search.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+            </div>
